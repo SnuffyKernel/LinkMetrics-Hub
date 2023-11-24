74ce6b44e6db298bc0da61c7715ca85942af00ca
Snuffy <49280702+SnuffyKernel@users.noreply.github.com>
2023-11-24 23:24:18 +0700

    Update statsService.js

--- urlShortService/service/statsService.js
+++ urlShortService/service/statsService.js
@@ -1,13 +1,15 @@
 const axios = require("axios")
 const IPHelper = require('../utils/IPHelper')
 
+const urlStatsService = 'http://stats_service:4000/' //for docker
+
 class StatsService {
     sendStatsService(req) {
         try{
             const timestamp = new Date().toISOString();
             const shortUrl = req.params.shortUrl;
             const ip = IPHelper.getClientIP(req);   
-            axios.post('http://localhost:4000/', {ip, shortUrl, timestamp}).catch(e => {console.error(e)})
+            axios.post(urlStatsService, {ip, shortUrl, timestamp}).catch(e => {console.error(e)})
         } catch(e) {
             console.log(e)
         }
